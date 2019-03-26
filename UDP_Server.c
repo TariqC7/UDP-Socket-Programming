48be99c51cf2773275ad1ed1f2922a1560ea7295
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-03-26 19:45:18 -0400

    Calling remaining libraries
    and initializing error function

--- UDP_Server.c
+++ UDP_Server.c
@@ -9,3 +9,17 @@
     #include <string.h>
     #include <stdlib.h>
     #include <sys/select.h>//use select() for multiplexing
+    #include <sys/fcntl.h> // for non-blocking
+    #define MAX_LENGTH 1024
+    /* Select() params
+     * int select(int n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
+     * FD_SET(int fd, fd_set *set);
+     * FD_CLR(int fd, fd_set *set);
+     * FD_ISSET(int fd, fd_set *set);
+     * FD_ZERO(fd_set *set);
+    */
+    void error(char *message)
+    {
+        perror(message);
+        exit(1);
+    }
