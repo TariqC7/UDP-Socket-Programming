4fa5fa2bdcd5747b13df71708d1fc60e06a4881d
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-03-26 19:38:50 -0400

    Adding all the libraries

--- UDP_Server.c
+++ UDP_Server.c
@@ -1,1 +1,11 @@
-
+//SERVER
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <errno.h>
+    #include <string.h>
+    #include <stdlib.h>
+    #include <sys/select.h>//use select() for multiplexing
