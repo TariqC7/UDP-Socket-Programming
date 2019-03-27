b1288c12f1ff09a3d40df86e19eca4479412fda9
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-03-26 21:39:11 -0400

    Including all the necessary libraries

--- UDP_Client.c
+++ UDP_Client.c
@@ -1,1 +1,10 @@
-
+//CLIENT
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h> //host struct
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
